6ba73f8140cfb82061a58a94c40fb4ad16657496
Jonas Bezerra <jonas.bezerra@sap.com>
2020-09-04 10:12:56 -0300

    create basic task

--- my-rake.rb
+++ my-rake.rb
@@ -1,5 +1,17 @@
 TASKS = {}
 
+class Task
+  def initialize(name, deps, action)
+    @name = name
+    @deps = deps
+    @action = action
+  end
+
+  def invoke
+
+  end
+end
+
 def task(name, deps=[], &block)
   TASKS[name] = Task.new(name, deps, block)
 end
